ca3fe04d297a1bdda09b4a6bda36fa1368774394
AkimJemi <rlawoals623@gmail.com>
2024-03-25 22:44:37 +0900

    20240325 8. String to Integer (atoi)
    Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
    
    The algorithm for myAtoi(string s) is as follows:
    
    Read in and ignore any leading whitespace.
    Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
    Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
    Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
    If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
    Return the integer as the final result.
    Note:
    
    Only the space character ' ' is considered a whitespace character.
    Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.

---
+++ src/String_To_Integer.java
@@ -0,0 +1,50 @@
+public class String_To_Integer {
+    public static void main(String[] args) {
+        System.out.println(new Solution2().myAtoi("+-12"));
+    }
+}
+
+class Solution2 {
+    public int myAtoi(String s) {
+        String result = "";
+        boolean check = true;
+        boolean minus = false;
+        boolean plus = false;
+        int notDouble = 0;
+        try {
+            for (Character cha : s.toCharArray()) {
+                if (Character.isDigit(cha)) {
+                    if (notDouble == 2) {
+                        return 0;
+                    }
+                    check = false;
+                    result += cha;
+                } else {
+                    if (notDouble == 2) {
+                        return minus ? Integer.valueOf("-" + result) : Integer.valueOf(result);
+                    }
+                    if (plus || minus) {
+                        return result.length() == 0 ? 0 : minus ? Integer.valueOf("-" + result) : Integer.valueOf(result);
+                    }
+                    if (String.valueOf(cha).equals(" ") && check) {
+                        continue;
+                    }
+                    if (String.valueOf(cha).equals("-") && check) {
+                        notDouble++;
+                        minus = true;
+                        continue;
+                    }
+                    if (String.valueOf(cha).equals("+") && check) {
+                        notDouble++;
+                        plus = true;
+                        continue;
+                    }
+                    return result.length() == 0 ? 0 : minus ? Integer.valueOf("-" + result) : Integer.valueOf(result);
+                }
+            }
+            return result.length() == 0 ? 0 : minus ? Integer.valueOf("-" + result) : Integer.valueOf(result);
+        } catch (Exception e) {
+            return minus ? -Integer.MAX_VALUE - 1 : Integer.MAX_VALUE;
+        }
+    }
+}
