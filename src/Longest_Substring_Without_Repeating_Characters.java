b4158eb989ac4f97de9d1ebf4b2b3c2e388446ac
AkimJemi <rlawoals623@gmail.com>
2024-03-26 23:05:52 +0900

    20240326 3. Longest Substring Without Repeating Characters
    Given a string s, find the length of the longest
    substring
     without repeating characters.
    
    Example 1:
    
    Input: s = "abcabcbb"
    Output: 3
    Explanation: The answer is "abc", with the length of 3.
    Example 2:
    
    Input: s = "bbbbb"
    Output: 1
    Explanation: The answer is "b", with the length of 1.
    Example 3:
    
    Input: s = "pwwkew"
    Output: 3
    Explanation: The answer is "wke", with the length of 3.
    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
    
    Constraints:
    
    0 <= s.length <= 5 * 104
    s consists of English letters, digits, symbols and spaces.

---
+++ src/Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Longest_Substring_Without_Repeating_Characters {
+    public static void main(String[] args) {
+        System.out.println(new Solution3().lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
+class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        List<String> strList = new ArrayList<>();
+        List<String> strResultList = new ArrayList<>();
+        String[] strArray = s.split("");
+        for (int i = 0; i < s.length(); i++) {
+            if (!strList.contains(strArray[i])) {
+                strList.add(strArray[i]);
+                if (strResultList.size() <= strList.size()) {
+                    strResultList = new ArrayList<>();
+                    strResultList.addAll(strList);
+                }
+                continue;
+            }
+            while (strList.contains(strArray[i])) {
+                for (int j = 0; j <= strList.indexOf(strArray[i]); j++) {
+                    strList.remove(0);
+                }
+            }
+            strList.add(strArray[i]);
+        }
+        return strResultList.size();
+    }
+}
