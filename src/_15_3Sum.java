244aa0a00f7f6acc527372368b44b6cd048137e7
AkimJemi <rlawoals623@gmail.com>
2024-03-31 20:23:05 +0900

    20240331 15. 3Sum 修正中 Time Limit Exceeded 311 / 313 testcases passed

---
+++ src/_15_3Sum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class _15_3Sum {
+    public static void main(String[] args) {
+        System.out.println(new Solution15().threeSum(new int[]{1,-1,-1,0}));
+
+    }
+
+}
+
+class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> setList = new HashSet<>();
+        Integer iP = null;
+        Integer jP = null;
+        for (int i = 0; i < nums.length; i++) {
+            int iVal = nums[i];
+            if (iP == null) iP = iVal;
+            else if (iVal == iP) continue;
+            iP = iVal;
+            for (int j = i + 1; j < nums.length; j++) {
+                int jVal = nums[j];
+                if (jP == null) jP = jVal;
+                else if (jVal == jP) continue;
+                jP = jVal;
+                for (int k = j + 1; k < nums.length; k++) {
+                    int kVal = nums[k];
+                    if (iVal + jVal + kVal == 0) {
+                        setList.add(Arrays.asList(iVal, jVal, kVal));
+                        break;
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(setList);
+    }
+}
