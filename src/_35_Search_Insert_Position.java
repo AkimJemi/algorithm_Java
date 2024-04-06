2c73477ad1a101aa87f7cb7256db6dce5c3208f3
AkimJemi <rlawoals623@gmail.com>
2024-04-06 15:11:54 +0900

    20240406 35. Search Insert Position

---
+++ src/_35_Search_Insert_Position.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _35_Search_Insert_Position {
+    public static void main(String[] args) {
+        System.out.println(new Solution35().searchInsert(new int[]{1, 3}, 2));
+    }
+}
+
+class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int numArrayLength = nums.length;
+        if (target > nums[numArrayLength - 1]) {
+            return numArrayLength;
+        }
+        if (target < nums[0]) {
+            return 0;
+        }
+        for (int i = 0; i < numArrayLength; i++) {
+            System.out.println(i);
+            if (nums[i] == target) {
+                return i;
+            }
+            if (i <= numArrayLength - 2) {
+                if (nums[i] < target && nums[i + 1] > target) {
+                    return i + 1;
+                }
+            }
+        }
+        return 0;
+    }
+}
