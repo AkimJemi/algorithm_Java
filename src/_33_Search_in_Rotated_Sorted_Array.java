7491d4d5d375befd9508d4cf806e98d33680eae7
AkimJemi <rlawoals623@gmail.com>
2024-04-06 14:32:09 +0900

    20240406 33. Search in Rotated Sorted Array

---
+++ src/_33_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _33_Search_in_Rotated_Sorted_Array {
+    public static void main(String[] args) {
+        System.out.println(new Solution33().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+    }
+}
+
+class Solution33 {
+    public int search(int[] nums, int target) {
+        return Arrays.stream(nums).boxed().collect(Collectors.toList()).indexOf(target);
+    }
+}
