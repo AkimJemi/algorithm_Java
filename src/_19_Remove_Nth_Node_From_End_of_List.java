78411bbf1ef53c955df12471026bc8fa55bd5164
AkimJemi <rlawoals623@gmail.com>
2024-04-13 19:58:58 +0900

    20240413 19. Remove Nth Node From End of List
    Given the head of a linked list, remove the nth node from the end of the list and return its head.
    
    Example 1:
    
    Input: head = [1,2,3,4,5], n = 2
    Output: [1,2,3,5]
    Example 2:
    
    Input: head = [1], n = 1
    Output: []
    Example 3:
    
    Input: head = [1,2], n = 1
    Output: [1]
    
    Constraints:
    
    The number of nodes in the list is sz.
    1 <= sz <= 30
    0 <= Node.val <= 100
    1 <= n <= sz

---
+++ src/_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,65 @@
+import org.w3c.dom.NodeList;
+
+import java.util.*;
+
+public class _19_Remove_Nth_Node_From_End_of_List {
+    public static void main(String[] args) {
+        ListNode node = new Solution19().removeNthFromEnd(List.of(1, 2, 3, 4, 5).stream().sorted(Comparator.reverseOrder()).map(a -> new ListNode(a)).reduce((a, b) -> {
+                    b.next = a;
+                    return b;
+                }
+        ).get(), 2);
+        int i = 0;
+        while (true) {
+            if (node.next == null) {
+                break;
+            }
+            node = node.next;
+        }
+    }
+}
+
+class Solution19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        List<Integer> intList = new ArrayList<>();
+        while (true) {
+            if (head != null) {
+                intList.add(head.val);
+            }
+            if (head.next == null) {
+                break;
+            }
+            head = head.next;
+        }
+        intList.remove(intList.size() - n);
+        Collections.reverse(intList);
+        Optional<ListNode> optional = intList.stream().map(a -> new ListNode(a)).reduce((a, b) -> {
+                    b.next = a;
+                    return b;
+                }
+        );
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            return null;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
