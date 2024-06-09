20ba6ac2f5ac80b260269db2e7868ec5f7a5d6fc
AkimJemi <rlawoals623@gmail.com>
2024-06-09 23:33:04 +0900

    20240609 LeetCode 15. 3Sum
    https://leetcode.com/problems/3sum/description/
    
    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
    
    Notice that the solution set must not contain duplicate triplets.
    
    Example 1:
    
    Input: nums = [-1,0,1,2,-1,-4]
    Output: [[-1,-1,2],[-1,0,1]]
    Explanation:
    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
    The distinct triplets are [-1,0,1] and [-1,-1,2].
    Notice that the order of the output and the order of the triplets does not matter.
    Example 2:
    
    Input: nums = [0,1,1]
    Output: []
    Explanation: The only possible triplet does not sum up to 0.
    Example 3:
    
    Input: nums = [0,0,0]
    Output: [[0,0,0]]
    Explanation: The only possible triplet sums up to 0.
    
    Constraints:
    
    3 <= nums.length <= 3000
    -105 <= nums[i] <= 105

--- src/LeetCode/_15_3Sum.java
+++ src/LeetCode/_15_3Sum.java
@@ -1,10 +1,11 @@
 package LeetCode;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class _15_3Sum {
     public static void main(String[] args) {
-        System.out.println(new Solution15().threeSum(new int[]{1,-1,-1,0}));
+        System.out.println(new Solution15().threeSum(new int[]{0, 0, 0}));
     }
 }
 
@@ -19,13 +20,16 @@ class Solution15 {
             if (iP == null) iP = iVal;
             else if (iVal == iP) continue;
             iP = iVal;
+            if (iVal > 0) break;
             for (int j = i + 1; j < nums.length; j++) {
                 int jVal = nums[j];
                 if (jP == null) jP = jVal;
                 else if (jVal == jP) continue;
                 jP = jVal;
+                if (iVal + jVal > 0) break;
                 for (int k = j + 1; k < nums.length; k++) {
                     int kVal = nums[k];
+                    if (iVal + jVal + kVal > 0) break;
                     if (iVal + jVal + kVal == 0) {
                         setList.add(Arrays.asList(iVal, jVal, kVal));
                         break;
@@ -35,4 +39,25 @@ class Solution15 {
         }
         return new ArrayList<>(setList);
     }
+    //    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> list = new ArrayList<>();
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length - 2; i++) {
+//            if (nums[i] > 0) break;
+//            for (int j = i + 1; j < nums.length - 1; j++) {
+//                if (nums[i] + nums[j] > 0) break;
+//                for (int n = j + 1; n < nums.length; n++) {
+//                    if (nums[i] + nums[j] + nums[n] > 0) break;
+//                    if (nums[i] + nums[j] + nums[n] == 0) {
+//                        int min = Math.min(Math.min(nums[i], nums[j]), nums[n]);
+//                        int max = Math.max(Math.max(nums[i], nums[j]), nums[n]);
+//                        int mid = nums[i] + nums[j] + nums[n] - max - min;
+//                        List<Integer> tmp = List.of(min, mid, max);
+//                        if (!list.contains(tmp)) list.add(tmp);
+//                    }
+//                }
+//            }
+//        }
+//        return list;
+//    }
 }
