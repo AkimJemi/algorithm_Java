61f5604b0212284cb3e010ccae0aba60496e7549
AkimJemi <rlawoals623@gmail.com>
2024-07-18 21:03:45 +0900

    20240718 88. Merge Sorted Array
    https://leetcode.com/problems/merge-sorted-array/submissions/
    
    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
    
    Example 1:
    
    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
    Output: [1,2,2,3,5,6]
    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
    Example 2:
    
    Input: nums1 = [1], m = 1, nums2 = [], n = 0
    Output: [1]
    Explanation: The arrays we are merging are [1] and [].
    The result of the merge is [1].
    Example 3:
    
    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
    Output: [1]
    Explanation: The arrays we are merging are [] and [1].
    The result of the merge is [1].
    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
    
    Constraints:
    
    nums1.length == m + n
    nums2.length == n
    0 <= m, n <= 200
    1 <= m + n <= 200
    -109 <= nums1[i], nums2[j] <= 109

--- src/LeetCode/_88_Merge_Sorted_Array.java
+++ src/LeetCode/_88_Merge_Sorted_Array.java
@@ -1,17 +1,56 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class _88_Merge_Sorted_Array {
     public static void main(String[] args) {
-        new Solution88().merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3);
+        new Solution88().merge2(new int[]{-1, 1, 0, 0, 0, 0, 0, 0}, 2, new int[]{-1, 0, 1, 1, 2, 3}, 6);
     }
 }
 
 class Solution88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            if (nums1[i] < nums2[count]) {
+        for (int i = n, j = 0; i > 0; j++) {
+            if (nums1[j] == 0) {
+                nums1[j] = nums2[nums2.length - i];
+                i--;
+            }
+        }
+        int start = 0;
+        int end = nums1.length - 1;
+        while (true) {
+            if (end == 0) break;
+            if (start == end) {
+                end--;
+                start = 0;
+            }
+            if (nums1[start] > nums1[start + 1]) {
+                int mid = nums1[start];
+                nums1[start] = nums1[start + 1];
+                nums1[start + 1] = mid;
+            }
+            start++;
+        }
+    }
 
+
+    // others code
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = m + n - 1;
+        int k = n - 1;
+        while (j >= 0 && k >= 0) {
+            if (i < 0) {
+                while (j >= 0 && k >= 0) {
+                    nums1[j--] = nums2[k--];
+                }
+            } else {
+                if (nums1[i] > nums2[k]) {
+                    nums1[j--] = nums1[i--];
+                }
+                else{
+                    nums1[j--] = nums2[k--];
+                }
             }
         }
     }
